1b81f04fc46b766f5cfe89675a77bc5deef2f835
Yersin <yersinme@gmail.com>
2025-02-18 21:53:08 +0500

    wsd

--- components/MainShipaSearch/ShipaMainHeader/ShipaMainHeader.vue
+++ components/MainShipaSearch/ShipaMainHeader/ShipaMainHeader.vue
@@ -10,7 +10,7 @@ const HeaderTogglestyleState = TogglestyleState
   <div class="shipa-main_header">
     <div class="shipa-main_header_title">Все санатории</div>
     <div class="shipa-main_header_style_btns">
-      <button @click="HeaderTogglestyleState" v-if="HeaderStyleState">
+      <!-- <button @click="HeaderTogglestyleState" v-if="HeaderStyleState">
         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368">
           <path d="M120-520v-320h320v320H120Zm0 400v-320h320v320H120Zm400-400v-320h320v320H520Zm0 400v-320h320v320H520ZM200-600h160v-160H200v160Zm400 0h160v-160H600v160Zm0 400h160v-160H600v160Zm-400 0h160v-160H200v160Zm400-400Zm0 240Zm-240 0Zm0-240Z" />
         </svg>
@@ -20,7 +20,7 @@ const HeaderTogglestyleState = TogglestyleState
         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368">
           <path d="M360-240h440v-107H360v107ZM160-613h120v-107H160v107Zm0 187h120v-107H160v107Zm0 186h120v-107H160v107Zm200-186h440v-107H360v107Zm0-187h440v-107H360v107ZM160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Z" />
         </svg>
-      </button>
+      </button> -->
     </div>
   </div>
 </template>
