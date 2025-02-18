93feb0c6d2c7e6d3bee2d2d46aa59f015efdfb44
Yersin <yersinme@gmail.com>
2025-02-18 21:32:44 +0500

    dk

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,49 @@
 <script setup lang="ts">
-import Header from '../components/Header/Header.vue'
-import Footer from "../components/Footer/Footer.vue"
+import { ref, onMounted, onUnmounted } from "vue";
+import Header from "../components/Header/Header.vue";
+
+const isHeaderVisible = ref(true);
+let lastScrollY = 0;
+
+const handleScroll = () => {
+  const currentScrollY = window.scrollY;
+  isHeaderVisible.value = currentScrollY < lastScrollY || currentScrollY <= 0;
+  lastScrollY = currentScrollY;
+};
+
+onMounted(() => {
+  window.addEventListener("scroll", handleScroll);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("scroll", handleScroll);
+});
 </script>
 
 <template>
   <div class="main">
-    <Header />
+    <header :class="{ hidden: !isHeaderVisible }">
+      <Header />
+    </header>
     <NuxtPage />
-    <!-- <Footer /> -->
   </div>
 </template>
-
 
 <style scoped>
 * {
   background-color: #252836;
 }
+
+header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 1000;
+  transition: transform 0.3s ease-in-out;
+}
+
+header.hidden {
+  transform: translateY(-100%); /* Скрываем header вверх */
+}
 </style>
-
-
